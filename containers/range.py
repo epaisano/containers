3d99f52431aa7d75ed59bc4c38b3cd01e1c3bb66
Emma Munoz <epaisano24@cmc.edu>
2023-03-06 15:25:45 -0800

    finished homework

--- containers/range.py
+++ containers/range.py
@@ -1,3 +1,5 @@
+
+
 def range(a, b=None, c=None):
     '''
     This function should behave exactly like the built-in range function.
@@ -9,21 +11,28 @@ def range(a, b=None, c=None):
     [1, 2, 3, 4]
     >>> list(range(1, 5, 2))
     [1, 3]
-
-    HINT:
-    If you can figure out how to use the built-in range function (without modifying the test cases!),
-    then feel free to do so.
-    That's fairly difficult to do, however, and it's much easier to just implement this function normally using the yield syntax.
-
-    NOTE:
-    For efficiency reasons, Python's built-in range object is written in the C programming language rather than natively in python.
-    You can find the source code online at https://hg.python.org/cpython/file/ee7b713fec71/Objects/rangeobject.c
-    The link takes you to a file that is 1445 lines long,
-    and all it does is implement this simple functionality.
-    My easy to read Python implementation of this function is just 15 lines long.
-    (And with some code golf I also wrote a less readable version that is only 4 lines.)
-    It is very common for C programs to be 100x longer than their corresponding python programs.
-    C code must manage lots of details about the computer manually that python code automates for you.
-    Carefully written C code can be faster than the corresponding python code because it can remove some of the overhead of this automation process,
-    but the resulting code is much longer and harder to read/write.
     '''
+    if b is None and c is None:
+        result = -1
+        i = 0
+        while i < a:
+            result += 1
+            i += 1
+            yield result
+    if b is not None and c is None:
+        result = a-1
+        i = a
+        while i < b:
+            result += 1
+            i += 1
+            yield result
+    if b is not None and c is not None:
+        if ((b < 0) and (c < 0)) or ((b > 0) and (c > 0)):
+            result = a - c
+            i = a
+            while abs(i) < abs(b):
+                result += c
+                i += c
+                yield result
+        else:
+            yield from ()
